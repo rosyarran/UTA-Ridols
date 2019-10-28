d87d016414bbc7228994839855cb1abe589e5d39
Rosy Arreola <rosy.arreola@yahoo.com>
2019-10-28 05:18:05 +0000

    simple form and new riddle

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -4,4 +4,19 @@ class RiddlesController < ApplicationController
     @riddle = Riddle.order("RANDOM()").first
   end
   
+  def new
+    @riddle = Riddle.new
+  end
+  
+  def create
+    Riddle.create(riddle_params)
+    redirect_to root_path
+  end
+  
+  private
+  
+  def riddle_params
+    params.require(:riddle).permit(:question, :answer)
+  end
+  
 end
